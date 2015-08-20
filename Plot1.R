daf2d38f4d573cbc25a5bb928183f33358dc3fa2
Calliope7 <7fergies@gmail.com>
2015-08-20 08:20:40 +0100

    Answered Q1

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+# National Emmissions Inventory (NEI)
+# 
+
+library(dplyr)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#Question: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+plot1 <- function() {
+        polByYear <- group_by(NEI, year)
+        totalEmissionsByYear <- summarise(polByYear, sum(Emissions, na.rm=TRUE))
+        names(totalEmissionsByYear)[2]<-"totalEmissions"
+        with(totalEmissionsByYear, plot(year, totalEmissions, ylab="Total Emissions"))
+        png(filename = "Plot1.png", width = 480, height = 480)
+        plot()
+        dev.off()
+}
